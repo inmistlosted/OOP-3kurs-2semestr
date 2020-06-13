2cfeba42b11af4b277dce93dfb61345384669a84
inmistlosted <43147435+inmistlosted@users.noreply.github.com>
2020-06-13 16:27:35 +0300

    Додав екзаменаційну роботу

---
+++ Exam/src/main/java/Main.java
@@ -0,0 +1,36 @@
+import entities.Triangle;
+import services.TriangleService;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Scanner scanner = new Scanner(System.in);
+        TriangleService service = new TriangleService();
+
+        System.out.println("Введіть площу: ");
+        double square1 = scanner.nextDouble();
+        Triangle triangle1 = service.findClosestSquare(square1);
+        System.out.println("Трикутник, площа якого найбільш наближена до площі " + square1 + ": " + triangle1);
+        System.out.println();
+
+        System.out.println("Введіть площу: ");
+        double square2 = scanner.nextDouble();
+        List<Triangle> triangles2 = service.findClosestSquareTrians(square2);
+        System.out.println("Трикутники, сума площ яких найбільш наближена до площі " + square2 + ":");
+        for (Triangle triangle : triangles2){
+            System.out.println(triangle);
+        }
+        System.out.println();
+
+        System.out.println("Введіть радіус: ");
+        double radius = scanner.nextDouble();
+        List<Triangle> triangles3 = service.findTriansWithLessRadius(radius);
+        System.out.println("Трикутники, які поміщаються в окружність з радіусом " + radius + ":");
+        for (Triangle triangle : triangles3){
+            System.out.println(triangle);
+        }
+    }
+}
